15b0dd42f6371bf74d3689709ea2a5b2131df5a2
Mr.Liu <liuyx.android@gmail.com>
2019-06-23 15:00:35 +0800

    修改方法

--- www/thsExportDoc.js
+++ www/thsExportDoc.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'thsExportDoc', 'coolMethod', [arg0]);
+exports.exportDoc = function (arg0, success, error) {
+    exec(success, error, 'thsExportDoc', 'exportDoc', [arg0]);
 };
