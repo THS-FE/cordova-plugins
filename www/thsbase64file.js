e0fc3b2eb452732bf9266538a4858c87b341f4ef
Mr.Liu <liuyx.android@gmail.com>
2019-09-05 09:36:15 +0800

    修改插件调用参数

--- www/thsbase64file.js
+++ www/thsbase64file.js
@@ -1,10 +1,10 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-    saveBase64ToFile: function (successCallback, errorCallback) {
+    saveBase64ToFile: function (base64Str,path,successCallback, errorCallback) {
         cordova.exec(
             successCallback,
             errorCallback,
-            "thsbase64file", "saveBase64ToFile", []);
+            "thsbase64file", "saveBase64ToFile", [base64Str,path]);
     }
 };
